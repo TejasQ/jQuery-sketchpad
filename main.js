7f20723631cca462099b7bb40707e63c93b2f3f7
Tejas Kumar <tejas@growqatar.com>
2015-01-28 12:02:35 +0300

    Created a settings object that can be used to customize the 'whiteboard'

--- main.js
+++ main.js
@@ -3,25 +3,31 @@
  * @name SketchPad
  * @desc Multi-touch sketchpad demo for HTML5 Canvas drawing
  */
+
+var settings = {
+
+        colors: { 
+          r: 0, //red stroke
+          g: 0, //green stroke
+          b: 0, //blue stroke
+          a: 1
+        }, //alpha opacity
+        started: false,
+        moved: false,
+        size: 10,
+        lines: [],
+        optionsOpen: false,
+        pixelRatio: 1
+
+      };
  
 var sketch = (function () {
 
 	var canvas, //canvas element.
 		ctx, //drawing context.
-		r = 0, //red stroke
-		g = 0, //green stroke
-		b = 0, //blue stroke
-		a = 0.5, //alpha opacity
-		started = false,
-		moved = false,
-		size = 1,
 		xPos,
-		yPos,
-		lines = [],
-		optionsOpen = false,
-		pixelRatio = 1,
-		hasLocalStorage = 'localStorage' in window && window['localStorage'] !== null;
-		
+		yPos;
+  
 	return {
 	
 		init: function () {
@@ -96,14 +102,14 @@ var sketch = (function () {
 		        };
 			}, this);
 			moved = false;
-			started = true;		
+			settings.started = true;		
 		},
 			
 		onTouchMove: function (e) {
 			
 			e.preventDefault();	
 			
-			if (started && !optionsOpen) {
+			if (settings.started && !optionsOpen) {
 				_.each(e.touches, function (touch) {
 				
 					var id = touch.identifier,
@@ -123,7 +129,7 @@ var sketch = (function () {
 			if (e.touches.length === 0) {
 				lines = [];
 				moved = false;
-				started = false;
+				settings.started = false;
 			}
 		},
 		
@@ -131,19 +137,19 @@ var sketch = (function () {
 			if (e.touches.length === 0) {
 				lines = [];
 				moved = false;
-				started = false;
+				settings.started = false;
 			}
 		},
 			
 		onMouseDown: function (e) {	
 			moved = false;
-			started = true;				
+			settings.started = true;				
 			canvas.addEventListener('mousemove', sketch.onMouseMove, false);
 			canvas.addEventListener('mouseup', sketch.onMouseUp, false);			
 		},
 			
 		onMouseMove: function (e) {
-			if (started && !optionsOpen) {
+			if (settings.started && !settings.optionsOpen) {
 				sketch.drawLine(e.clientX, e.clientY);
 			}
 			moved = true;	
@@ -152,7 +158,7 @@ var sketch = (function () {
 		onMouseUp: function (e) {
 			sketch.endDraw();
 			moved = false;
-			started = false;
+			settings.started = false;
 			canvas.removeEventListener('mousemove', sketch.onMouseMove, false);
 			canvas.removeEventListener('mouseup', sketch.onMouseUp, false);		
 		},
@@ -173,8 +179,8 @@ var sketch = (function () {
 		
 			ctx.lineCap = 'round';
 			ctx.lineJoin = 'round';
-			ctx.lineWidth = size; 
-			ctx.strokeStyle = 'rgba(' + r + ',' + g + ',' + b + ',' + a + ')'; 
+			ctx.lineWidth = settings.size; 
+			ctx.strokeStyle = 'rgba(' + settings.colors.r + ',' + settings.colors.g + ',' + settings.colors.b + ',' + settings.colors.a + ')'; 
 			ctx.globalCompositeOperation = 'source-over';
 			ctx.beginPath();
 		    ctx.moveTo(xPos, yPos);
@@ -194,7 +200,7 @@ var sketch = (function () {
 		drawMulti: function (id, moveX, moveY) {
 			ctx.lineCap = 'round';
 			ctx.lineJoin = 'round';
-			ctx.lineWidth = size; 
+			ctx.lineWidth = settings.size; 
 			/*var grad1 = ctx.createLinearGradient(0, 0, window.innerWidth, window.innerHeight);
 			grad1.addColorStop(0,    'yellow');
 			grad1.addColorStop(0.25, 'red');
